03bddecf585e79e3a51d882d611b7b1fa2746d2b
nem0z <maxime.bernard.dev@gmail.com>
2023-08-28 17:15:57 +0200

    feat (src/app): init basic layout

--- app/app/src/app/layout.tsx
+++ app/app/src/app/layout.tsx
@@ -2,6 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+// components
+import Header from '../components/header/header';
+import Footer from '../components/footer/footer';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
